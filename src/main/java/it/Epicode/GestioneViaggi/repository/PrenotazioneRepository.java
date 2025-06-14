2c6146ce5d938ad3f49d394f817c280c56a35d03
MicheleFamoso <michelefamoso@gmail.com>
2025-06-14 11:12:11 +0200

    .

--- src/main/java/it/Epicode/GestioneViaggi/repository/PrenotazioneRepository.java
+++ src/main/java/it/Epicode/GestioneViaggi/repository/PrenotazioneRepository.java
@@ -1,7 +1,13 @@
 package it.Epicode.GestioneViaggi.repository;
 
+import it.Epicode.GestioneViaggi.model.Dipendente;
 import it.Epicode.GestioneViaggi.model.Prenotazione;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione,Integer> {
+
+
+    boolean existsByDipendenteAndDataRichiesta(Dipendente dipendente, LocalDate dataRichiesta);
 }
--- src/main/java/it/Epicode/GestioneViaggi/service/PrenotazioneService.java
+++ src/main/java/it/Epicode/GestioneViaggi/service/PrenotazioneService.java
@@ -1,4 +1,80 @@
 package it.Epicode.GestioneViaggi.service;
 
+
+import it.Epicode.GestioneViaggi.dto.PrenotazioneDto;
+import it.Epicode.GestioneViaggi.exception.NonTrovatoException;
+import it.Epicode.GestioneViaggi.model.Dipendente;
+import it.Epicode.GestioneViaggi.model.Prenotazione;
+import it.Epicode.GestioneViaggi.model.Viaggio;
+import it.Epicode.GestioneViaggi.repository.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PrenotazioneService {
+
+    @Autowired
+    PrenotazioneRepository prenotazioneRepository;
+
+    @Autowired
+    DipendenteService dipendenteService;
+
+    @Autowired
+    ViaggioService viaggioService;
+
+    public Prenotazione savePrenotazione(PrenotazioneDto prenotazioneDto) throws NonTrovatoException {
+        Dipendente dipendente = dipendenteService.getDipendente(prenotazioneDto.getDipendenteId());
+        Viaggio viaggio = viaggioService.getViaggio(prenotazioneDto.getViaggioId());
+
+        Prenotazione prenotazione = new Prenotazione();
+        prenotazione.setDataRichiesta(prenotazioneDto.getDataRichiesta());
+        prenotazione.setPreferenze(prenotazioneDto.getPreferenze());
+        prenotazione.setDipendente(dipendente);
+        prenotazione.setViaggio(viaggio);
+        boolean exists = prenotazioneRepository.existsByDipendenteAndDataRichiesta(dipendente, prenotazioneDto.getDataRichiesta());
+        if (exists) {
+            throw new IllegalStateException("Il dipendente ha già una prenotazione per questo giorno.");
+        }
+        return prenotazioneRepository.save(prenotazione);
+    }
+
+    public List<Prenotazione> getPrenotazioni(){
+        return prenotazioneRepository.findAll();
+    }
+
+    public Prenotazione getPrenotazione(int id) throws NonTrovatoException {
+        return prenotazioneRepository.findById(id).
+                orElseThrow(()->new NonTrovatoException("Prenotazione non trovata"));
+    }
+
+    public Prenotazione updatePrenotazione(int id, PrenotazioneDto prenotazioneDto) throws NonTrovatoException {
+        Prenotazione prenotazione = getPrenotazione(id);
+        prenotazione.setDataRichiesta(prenotazioneDto.getDataRichiesta());
+        prenotazione.setPreferenze(prenotazioneDto.getPreferenze());
+
+        if (prenotazioneDto.getDipendenteId()!= prenotazione.getDipendente().getId()){
+            Dipendente dipendente = dipendenteService.getDipendente(prenotazioneDto.getDipendenteId());
+            prenotazione.setDipendente(dipendente);
+        }
+        if (prenotazioneDto.getViaggioId()!= prenotazione.getViaggio().getId()){
+            Viaggio viaggio = viaggioService.getViaggio(prenotazioneDto.getViaggioId());
+            prenotazione.setViaggio(viaggio);
+        }
+        boolean exists = prenotazioneRepository.existsByDipendenteAndDataRichiesta(prenotazione.getDipendente(), prenotazioneDto.getDataRichiesta());
+        if (exists && !prenotazione.getDataRichiesta().equals(prenotazioneDto.getDataRichiesta())) {
+            throw new IllegalStateException("Il dipendente ha già una prenotazione per questo giorno.");
+        }
+        return prenotazioneRepository.save(prenotazione);
+    }
+
+    public void deletePrenotazione(int id) throws NonTrovatoException {
+        Prenotazione prenotazioneX = getPrenotazione(id);
+
+        prenotazioneRepository.delete(prenotazioneX);
+    }
+
 }
+
+
